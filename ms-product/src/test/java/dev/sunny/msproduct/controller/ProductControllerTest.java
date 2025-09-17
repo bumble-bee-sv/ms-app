179af05982b2a1d000bffb86ace7e8117ba2a33a
sv-newmac <ssunnyvvats@outlook.com>
2025-09-18 01:24:25 +0530

    Fix for Product API and test framework
     - Add fix for Product and Category API to return ordered response.
     - Add fix for Unit tests for Product APIs.
     - Modify pom to configure Mockito not to self-attach to JVM.

---
+++ ms-product/src/test/java/dev/sunny/msproduct/controller/ProductControllerTest.java
@@ -0,0 +1,92 @@
+package dev.sunny.msproduct.controller;
+
+import dev.sunny.msproduct.dto.ProductDto;
+import dev.sunny.msproduct.entity.Product;
+import dev.sunny.msproduct.mappers.ProductMapper;
+import dev.sunny.msproduct.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+@SpringBootTest
+class ProductControllerTest {
+
+    MockMvc mockMvc;
+
+    @Autowired
+    ProductController productController;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductMapper productMapper;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    void addProduct() {
+
+        ProductDto productDto = ProductDto.builder()
+                .title("Test Product")
+                .price(new BigDecimal(99))
+                .description("This is a test product")
+                .category("Test Category")
+                .image("https://example.com/image.jpg")
+                .build();
+
+        ProductDto savedProduct = productController.addProduct(productDto);
+
+        Assertions.assertNotNull(savedProduct, "Saved product should not be null");
+        Assertions.assertNotNull(savedProduct.getUniqueId(), "Saved product ID should not be null");
+
+        Product productEntity = productRepository.findById(savedProduct.getUniqueId()).orElse(null);
+        Assertions.assertNotNull(productEntity, "Product entity should not be null");
+        Assertions.assertNotNull(productEntity.getCategory(), "Product category should not be null");
+
+    }
+
+    @Test
+    @Transactional
+    void getProductById() {
+
+        ProductDto rawProductDto = ProductDto.builder()
+                .title("Test Product")
+                .price(new BigDecimal(99))
+                .description("This is a test product")
+                .category("Test Category")
+                .image("https://example.com/image.jpg")
+                .build();
+        Product savedProductEntity = productRepository.save(productMapper.toEntity(rawProductDto));
+        Assertions.assertNotNull(savedProductEntity, "Saved product should not be null");
+
+        Product product = productRepository.findById(1L).orElseThrow(RuntimeException::new);
+        ProductDto productDto = productController.getProductById(1L);
+
+        Assertions.assertEquals(product.getId(), productDto.getUniqueId(), "Product ID should be equal");
+        Assertions.assertEquals(product.getTitle(), productDto.getTitle(), "Product title should be equal");
+
+        Assertions.assertThrows(RuntimeException.class, () -> productRepository.findById(Random.from(RandomGenerator.of("L64")).nextLong(100, 1000))
+                .orElseThrow(RuntimeException::new), "Should throw exception for non-existing product");
+
+    }
+}
