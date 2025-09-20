a8a64f80c9bbc7db3cabdb60ec67854ae85c4c2e
sv-newmac <ssunnyvvats@outlook.com>
2025-09-20 21:49:30 +0530

    Add fix for Product business logic.
     - Add new method for finding all non-deleted products.
     - Add new method for soft deleting a product.
     - Refactor response, to have category name and in order.

--- ms-product/src/main/java/dev/sunny/msproduct/repository/ProductRepository.java
+++ ms-product/src/main/java/dev/sunny/msproduct/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package dev.sunny.msproduct.repository;
 
 import dev.sunny.msproduct.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,11 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<List<Product>> findAllByCategoryName(String name);
+
+    List<Product> findAllByDeleted(boolean deleted);
+
+    @Modifying
+    @Query("UPDATE Product p SET p.deleted = true WHERE p.id = ?1")
+    void markProductAsDeleted(Long id);
 }
 
