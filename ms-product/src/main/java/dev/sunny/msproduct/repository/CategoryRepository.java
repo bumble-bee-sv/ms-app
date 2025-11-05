1795f83845bc42f81d7a676c3ad825e1152eb58c
sv-newmac <ssunnyvvats@outlook.com>
2025-11-05 12:40:21 +0530

    ECOM-23 | ECOM Create GET all categories API
     - Add GET API for fetching all categories in system.
     - Add GET API for fetching all category names.
     - Add parameter deleted, with default false, to view categories based on deleted status.

--- ms-product/src/main/java/dev/sunny/msproduct/repository/CategoryRepository.java
+++ ms-product/src/main/java/dev/sunny/msproduct/repository/CategoryRepository.java
@@ -2,8 +2,10 @@ package dev.sunny.msproduct.repository;
 
 import dev.sunny.msproduct.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +16,10 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByNameAndDeleted(String name, boolean deleted);
 
     Optional<Category> findByNameAndDeletedFalse(String categoryName);
+
+    List<Category> findCategoriesByDeleted(boolean deleted);
+
+    @Query("SELECT c.name FROM Category c WHERE c.deleted = :deleted")
+    List<String> findCategoryNamesByDeleted(boolean deleted);
+
 }
